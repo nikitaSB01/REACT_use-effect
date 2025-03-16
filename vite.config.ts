93b70e11c26d0905578dc209aeaafcf93aa8d1d2
nikitaSB01 <boss.shvabauer@bk.ru>
2025-03-16 17:21:39 +0500

    add pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/REACT_use-effect/", // 🔥 путь на GitHub Pages
   plugins: [react()],
-})
+});
